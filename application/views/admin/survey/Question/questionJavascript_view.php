9da9f085608a81fe4763d8e416b7c7bcebf749ba
Kevin Duane <duank001@apps.disney.com>
2018-08-21 14:14:58 -0700

    helm chart created

--- application/views/admin/survey/Question/questionJavascript_view.php
+++
@@ -1,6 +0,0 @@
-<?php
-App()->getClientScript()->registerScript("ZOrder-EditQuestionView_question_jsviews" ,"
-if(window.questionFunctions) {
-    window.questionFunctions.OtherSelection('".$type."');
-}
-", LSYii_ClientScript::POS_POSTSCRIPT );
---
+++ kubernetes/helm/infra/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region = "us-west-2"
+db_username = "survey_admin"
+db_password = "Wh4tsTh4tSme11"
+subnet_ids = ["subnet-010e5266","subnet-28e89c61","subnet-5bee2b00"]
+skip_final_snapshot = true
